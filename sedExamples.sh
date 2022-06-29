fc8ed4debb3566c3b8391b310082bf936d7019ed
Chang-Hsing Lee <93421340+ChangHsingLee@users.noreply.github.com>
2022-06-29 11:09:04 +0800

    Update sedExamples.sh

--- sedExamples.sh
+++ sedExamples.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
-# Set first character in each word to uppercase and others to lowercase
+# Set first character in each word to uppercase and others to lowercase and remove whitespace
 # This captures two groups for each word (a word character, followed by zero or more word characters). 
 # The replacement uses \U to upper-case the first group and \L to lower-case the rest.
 # \w is shorthand for [[:alnum:]_], i.e. anything considered to be a letter or digit in your locale, plus _.
+echo "abc3 def gg3" | sed -E 's/(\w)(\w*)/\U\1\L\2/g; s/ //g'
+# Set first character in each word to uppercase and others to lowercase and charcter '.'
 echo "abc3.def.gg3" | sed -E 's/(\w)(\w*)/\U\1\L\2/g; s/\.//g'
-echo "abc3 def gg3" | sed -E 's/(\w)(\w*)/\U\1\L\2/g; s/\.//g'
+
+# change character ':', '/' or '-' to '.' in string
+echo "test/string-1234" | sed 's/[\/:-]/\./g'
