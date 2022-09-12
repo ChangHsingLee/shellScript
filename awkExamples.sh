df2128ab4ac12fbdd137339d5a1fab7e9b5f70ac
Chang-Hsing Lee <93421340+ChangHsingLee@users.noreply.github.com>
2022-09-12 09:50:13 +0800

    Update awkExamples.sh

--- awkExamples.sh
+++ awkExamples.sh
@@ -4,3 +4,5 @@
 awk -i inplace 'NR==3{$1="'$flashImg'"};1' $CONF_FILE
 # ignore line 1 which output from command 'docker images', and output 'column1:column2' of each line if colume1='ubuntu' and column2='xenial'.
 docker images | awk 'NR>1 && $1=="ubuntu" && $2=="xenial" {printf "%s:%s\n", $1, $2}'
+# show last field
+awk '{print $NF}'
