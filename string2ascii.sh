eab3b5f23f0c85889082794de3e66633de24519f
Chang-Hsing Lee <93421340+ChangHsingLee@users.noreply.github.com>
2023-04-15 17:09:20 +0800

    Create string2ascii.sh

---
+++ string2ascii.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+str2ASCII() {
+        local strLen
+        local i
+        strLen=${#1}
+        i=0
+        while [ $strLen -ge 1 ]; do
+                printf "0x%02X " \'${1:$i:1}
+                i=$((i+1))
+                strLen=$((strLen-1))
+        done
+}
