e474bb4f66b4fe1124a3da2f28419c4a53761d02
Chang-Hsing Lee <93421340+ChangHsingLee@users.noreply.github.com>
2023-08-24 17:14:58 +0800

    Update awkExamples.sh

--- awkExamples.sh
+++ awkExamples.sh
@@ -8,3 +8,6 @@ docker images | awk 'NR>1 && $1=="ubuntu" && $2=="xenial" {printf "%s:%s\n", $1,
 awk '{print $NF}'
 # calculate CPU usage
 awk '/cpu /{usage=100-($5*100)/($2+$3+$4+$5+$6+$7+$8)} END {print usage}' /proc/stat
+# calculate memory usage
+free | awk 'FNR==2 { if ($7 == 0) usage=($2-($4+$6))*100/$2; else usage=($2-$7)*100/$2;} END { printf ("%.2f\%\n",usage) }'
+
